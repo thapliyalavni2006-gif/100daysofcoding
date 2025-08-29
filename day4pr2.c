13a0721dec46c45d008f5f66421334ade7750d9b
Avni <thapliyal.avni2006@gmail.com>
2025-08-29 22:02:42 +0530

    Add files via upload

---
+++ day4pr2.c
@@ -0,0 +1,11 @@
+// Q8: Write a program to find and display the sum of the first n natural numbers.
+#include <stdio.h>
+int main()
+{
+    int n, sum;
+    printf("Enter value of n");
+    scanf("%d", &n);
+    sum = n * (n + 1) / 2;
+    printf("Sum of first n natural numebers is %d ", sum);
+    return 0;
+}
