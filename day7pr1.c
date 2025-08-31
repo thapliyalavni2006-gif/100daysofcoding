b5c19a5642458884a2d43e90c8c5f281f2b6c7b2
Avni <thapliyal.avni2006@gmail.com>
2025-08-31 12:39:22 +0530

    Add files via upload

---
+++ day7pr1.c
@@ -0,0 +1,18 @@
+// Q13: Write a program to input a year and check whether it is a leap year or not using conditional statements.
+
+#include <stdio.h>
+int main()
+{
+    int year;
+    printf("Enter the year");
+    scanf("%d", &year);
+    if (year % 4 == 0)
+    {
+        printf("Leap year");
+    }
+    else
+    {
+        printf("Not a leap year");
+    }
+    return 0;
+}
