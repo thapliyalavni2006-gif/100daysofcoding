a47126d0ceb6880f0483edbff4ee3dbb5ad526cc
Avni <thapliyal.avni2006@gmail.com>
2025-09-06 19:20:41 +0530

    Update day13pr1.c

--- day13pr1.c
+++ day13pr1.c
@@ -1,23 +1,5 @@
 // Q25: Write a program to implement a basic calculator using switch-case for +, -, *, /, %.
 
-/*
-Sample Test Cases:
-Input 1:
-4 2 +
-Output 1:
-6
-
-Input 2:
-10 3 %
-Output 2:
-1
-
-Input 3:
-15 5 /
-Output 3:
-3
-
-*/
 #include <stdio.h>
 int main()
 {
@@ -56,4 +38,5 @@ int main()
 		break;
 	}
 	return 0;
+
 }
