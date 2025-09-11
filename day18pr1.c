730adc31327f3e40d92d040727c1ea6706e740bb
Avni <thapliyal.avni2006@gmail.com>
2025-09-11 09:17:46 +0530

    Add files via upload

---
+++ day18pr1.c
@@ -0,0 +1,17 @@
+// Q35: Write a program to print all factors of a given number.
+
+#include <stdio.h>
+int main()
+{
+	int n;
+	printf("Enter number whose factors you want to display ");
+	scanf("%d", &n);
+	for (int i = 1; i <= n; i++)
+	{
+		if (n % i == 0)
+		{
+			printf("%d\t", i);
+		}
+	}
+	return 0;
+}
