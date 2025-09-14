2b86c747caf23d3a2ab930875ceeb504965b3e79
Avni <thapliyal.avni2006@gmail.com>
2025-09-14 23:58:53 +0530

    Add files via upload

---
+++ day21pr1.c
@@ -0,0 +1,10 @@
+// Q41: Write a program to swap the first and last digit of a number.
+#include <stdio.h>
+#include <math.h>
+int main()
+{
+	int n, first, end, swap, digit, divide;
+	printf("enter number ");
+	scanf("%d", &n);
+	digit = log10(n);
+}
