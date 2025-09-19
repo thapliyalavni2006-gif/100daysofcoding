eb82a6fc8332d76f92a20813ae1758b829b9294b
Avni <thapliyal.avni2006@gmail.com>
2025-09-19 12:15:43 +0530

    Add files via upload

---
+++ day26pr2.c
@@ -0,0 +1,37 @@
+//Q52: Write a program to print the following pattern:
+
+/*
+*
+
+*
+*
+*
+
+*
+*
+*
+*
+*
+
+*
+*
+*
+
+*
+
+*/
+#include <stdio.h>
+
+int main()
+ {
+    printf("*\n");
+    printf("\n");
+    printf("*\n*\n*\n");
+    printf("\n");
+    printf("*\n*\n*\n*\n*\n");
+    printf("\n");
+    printf("*\n*\n*\n");
+    printf("\n");
+    printf("*\n");
+     return 0;
+}
