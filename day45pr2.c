5ffed9e59083adb15ea5fdea44fa5e4765118c2e
Avni <thapliyal.avni2006@gmail.com>
2025-10-08 20:24:01 +0530

    Add files via upload

---
+++ day45pr2.c
@@ -0,0 +1,21 @@
+//Q90: Toggle case of each character in a string.
+
+#include <stdio.h>
+#include <string.h>
+int main() {
+    char str[100];
+    int i;
+	printf("Enter a string: ");
+    fgets(str,100,stdin);
+
+    for (i=0; str[i]!='\0';i++) {
+        if (str[i]>='A' && str[i]<='Z') {
+            str[i]=str[i]+32;
+        }
+        else if (str[i]>='a' && str[i]<='z') {
+            str[i]=str[i]-32;
+        }
+    }
+  printf("Toggled case string: %s", str);
+ return 0;
+}
